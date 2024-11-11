b6305def81e29f3eae85b0b461f20114f0c3c722
MoBa1999 <mofriba@gmail.com>
2024-11-11 13:51:18 +0000

    Neue Trainingsdaten

--- Attention_Test/check_training_data.py
+++ Attention_Test/check_training_data.py
@@ -1,18 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os 
+import subprocess
+import pyslow5
+
+
+def plot_squigulator(fasta_folder, blow5_folder, reads_per_sequence):
+
+    
+    input_file = f"{fasta_folder}/fasta_file_{4}.fasta"
+    for j in range (reads_per_sequence):
+        output_file = f"{blow5_folder}/seq_{2}_read_{j}.blow5"
+        # Command to be executed
+        if j != 0:
+            command = ["/workspaces/MoBa_FP/Squigulator/squigi/squigulator", "-x", "dna-r9-min", input_file, "-o", output_file, "-n", "1", "--seed", str(j+1)]
+        else:
+            command = ["/workspaces/MoBa_FP/Squigulator/squigi/squigulator", "-x", "dna-r9-min", input_file, "-o", output_file, "-n", "1", "--ideal", "--seed", str(j+1)]
+        
+
+        # Run the command
+        subprocess.run(command, check=True)
+        s5 = pyslow5.Open(output_file, 'r')
+        reads = s5.seq_reads()
+        for read in reads:
+            signal = read['signal']
+            plt.plot(signal)
+
+fasta_dir = "/media/hdd1/MoritzBa/Rd_Data_Fasta"
+example_folder = "/workspaces/MoBa_FP/Training_Data_Example"
+plot_squigulator(fasta_dir,example_folder,1)
 
 signals = []
 seqs = []
 data_path = "/media/hdd1/MoritzBa/Rd_Data_Numpy"
 
-sequence = 1 #7000 zum trainieren
-num_reads = 10
-
-print("Start")
-
-for j in range(num_reads):
+sequence = 4 #7000 zum trainieren
+num_reads = [1]
+for j in num_reads:
     # Load signal and pad to max_length
     signal = np.load(f"{data_path}/signal_seq_{sequence}_read_{j}.npy")
     print(signal)
+    plt.plot(signal)
+
+
+
+plt.xlim(0,200)
+plt.show()
         
 
--- Attention_Test/train_random_data.py
+++ Attention_Test/train_random_data.py
@@ -6,48 +6,14 @@ import os
 import torch 
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
+from data_prep_func import get_data_loader
 
 
 
-signals = []
-seqs = []
 data_path = "/media/hdd1/MoritzBa/Rd_Data_Numpy"
 
-num_sequences = 7000 #7000 zum trainieren
-num_reads = 10
+max_length, train_loader = get_data_loader(data_path,1000)
 
-# Find the maximum length across all signals for padding
-max_length = 0
-for i in range(num_sequences):
-    for j in range(num_reads):
-        signal = np.load(f"{data_path}/signal_seq_{i}_read_{j}.npy")
-        max_length = max(max_length, signal.shape[0])
-print(f"{max_length} is the longest length of a read")
-
-# Load, pad, and store signals and sequences
-for i in range(num_sequences):
-    #List initialized
-    sequence_signals = []
-    for j in range(num_reads):
-        # Load signal and pad to max_length
-        signal = np.load(f"{data_path}/signal_seq_{i}_read_{j}.npy")
-        padding_length = max_length - signal.shape[0]
-        padded_signal = np.pad(signal, (0, padding_length), mode='constant', constant_values=0)
-        sequence_signals.append(padded_signal)
-        
-    # Load target sequence
-    signals.append(sequence_signals)
-    seq = np.load(f"{data_path}/signal_seq_{i}_read_{0}_tarseq.npy")
-    seqs.append(seq)
-
-
-# Convert lists to arrays
-signals = torch.from_numpy(np.array(signals))
-seqs = torch.from_numpy(np.array(seqs))
-
-signals = signals.view(signals.shape[0], signals.shape[1], signals.shape[2], 1).float()
-print(signals.shape)
-print(seqs.shape)
 model = MultiSeqModel(max_length, 200)
 
 # input_tensor_example = tf.convert_to_tensor(signal.reshape((1, len(signals[0]), 1)), dtype=tf.float32)
@@ -57,8 +23,7 @@ model = MultiSeqModel(max_length, 200)
 
 model = MultiSeqModel(input_length=max_length, tar_length=200)
 print(model.get_num_params())
-dataset = TensorDataset(signals, seqs)
-train_loader = DataLoader(dataset, batch_size=64, shuffle=True)
+
 model.train_model(train_loader, num_epochs=100, learning_rate=0.001)
 
 
