79761850efcf2cd8994a45a30d26f4938ddf28df
MoBa1999 <mofriba@gmail.com>
2024-11-13 10:56:30 +0000

    Updates

--- Attention_Test/basic_train.py
+++ Attention_Test/basic_train.py
@@ -9,14 +9,14 @@ from data_prep_func import get_data_loader
 from data_prep_func import get_device
 
 
-device = get_device(gpu_index=2)
+device = get_device(gpu_index=1)
 
 data_path = "/media/hdd1/MoritzBa/Rd_Data_Numpy"
-max_length, train_loader = get_data_loader(data_path,100, batch_size = 4, num_reads=1)
+max_length, train_loader = get_data_loader(data_path,10, batch_size = 2, num_reads=1)
 
 
 model = BasicModel(input_length=max_length, tar_length=200,d_model = 64, cnn_blocks = 3, max_pool_id = 1)
     
     # Train model and get losses and accuracies
-losses, accuracies = model.train_model(train_loader, num_epochs=100, learning_rate=0.001, device=device)
+losses, accuracies = model.train_model(train_loader, num_epochs=100, learning_rate=0.1, device=device)
 
